f4a2a1574d86c17294c2a41a10d2f2db9a47ab67
malalatiseira <tiseiramarialaura@gmail.com>
2022-09-28 13:14:06 -0300

    Updated

---
+++ Ej 12(JavaScrip)/Act. Clase/script.js
@@ -0,0 +1,10 @@
+let vehículo = {
+    marca: 'Opel',
+    color: 'negro',
+    numeroRuedas: 4,
+    numeroPlazas: 5,
+    tipo:'deportivo',
+    precio: 1000,
+};
+
+alert ('El vehículo con marca' + 'vehículo marca' + 'de color' + vehículo.color + ' tiene un coste de ' + vehículo.precio + ' $.')
